716718f6584bfea10a11f7faa903e758570ae68b
Jesse Eichar <Jesse.Eichar@swisscom.com>
2017-03-03 20:04:34 +0100

    Add create polygon to the geomtry builder

--- builder.go
+++ builder.go
@@ -31,6 +31,7 @@ func (err *BuilderError) String() string {
 type part struct {
 	partType partType
 	data     []float64
+	ends     []int
 }
 
 type partType int
@@ -68,6 +69,17 @@ func (b *Builder) StartLine(coord ...float64) *Builder {
 	return b.startGeom("StartLine", lineType, coord, true)
 }
 
+func (b *Builder) StartPolygon(coord ...float64) *Builder {
+	return b.startGeom("StartPolygon", polygonType, coord, true)
+}
+
+func (b *Builder) StartHole(coord ...float64) *Builder {
+	b.CloseRing()
+	b.validateNextCoord("StartHole", coord)
+	b.current.data = append(b.current.data, coord...)
+	return b
+}
+
 func (b *Builder) LineTo(coord ...float64) *Builder {
 	b.validateNextCoord("LineTo", coord)
 	if b.err != nil {
@@ -75,13 +87,11 @@ func (b *Builder) LineTo(coord ...float64) *Builder {
 	}
 	if b.current.partType == nullType {
 		b.err = &BuilderError{"LineTo() cannot be executed without first starting a (non-point) geometry.", debug.Stack()}
+		return b
 	}
 
 	if b.current.partType == pointType {
 		b.err = &BuilderError{"LineTo() cannot be executed on a point geometry.", debug.Stack()}
-	}
-
-	if b.err != nil {
 		return b
 	}
 
@@ -96,41 +106,130 @@ func (b *Builder) AddLineSegments(coords ...Coord) *Builder {
 	return b
 }
 func (b *Builder) CloseRing() *Builder {
+	if b.err != nil {
+		return b
+	}
+	if b.current.partType == nullType {
+		b.err = &BuilderError{"A geometry must be started before it can be closed", debug.Stack()}
+		return b
+	}
+	if len(b.current.data)/b.layout.Stride() < 3 {
+		b.err = &BuilderError{"CloseRing() can only be called if the current geometry has at least 3 coordinates", debug.Stack()}
+		return b
+	}
+
+	lastEnd := 0
+	if len(b.current.ends) > 0 {
+		lastEnd = b.current.ends[len(b.current.ends)-1]
+	}
+
+	firstCoord := Coord(b.current.data[lastEnd : lastEnd+b.layout.Stride()])
+	lastCoord := Coord(b.current.data[len(b.current.data)-b.layout.Stride():])
+
+	if !firstCoord.Equal(b.layout, lastCoord) {
+		b.current.data = append(b.current.data, firstCoord...)
+		end := len(b.current.data)
+		b.current.ends = append(b.current.ends, end)
+	}
+
 	return b
 }
 func (b *Builder) Point() (point *Point, err *BuilderError) {
+	b.validateGeomType("Point", pointType)
 	if b.err != nil {
 		return nil, b.err
-	}
-	if b.current.partType != pointType {
-		return nil, &BuilderError{fmt.Sprintf("A point cannot be created because the current geometry under construction is a '%v'.", b.current.partType), debug.Stack()}
 	}
 	return NewPointFlat(b.layout, b.current.data), nil
 }
-func (b *Builder) MultiPoint() (points *MultiPoint, err *BuilderError) {
-	return nil, nil
-}
-func (b *Builder) LineString() (line *LineString, err *BuilderError) {
+func (b *Builder) MultiPoint(allGeoms bool) (points *MultiPoint, err *BuilderError) {
 	if b.err != nil {
 		return nil, b.err
 	}
 
-	if b.current.partType != lineType {
-		return nil, &BuilderError{fmt.Sprintf("A LineString cannot be created because the current geometry under construction is a '%v'.", b.current.partType), debug.Stack()}
+	i := 0
+	if !allGeoms {
+		i = len(b.parts)
+		for ; i >= 0 && b.parts[i-1].partType == pointType; i-- {
+		}
 	}
 
+	coords := make([]float64, 0, len(b.parts)-i+1)
+
+	for ; i < len(b.parts); i++ {
+		if b.parts[i].partType != pointType {
+			b.err = &BuilderError{fmt.Sprintf("The geometry at index %d was not a point it was a '%v'", i, b.parts[i].partType), debug.Stack()}
+			return nil, b.err
+		}
+		coords = append(coords, b.parts[i].data...)
+	}
+
+	b.validateGeomType("Point", pointType)
+
+	if b.err != nil {
+		return nil, b.err
+	}
+
+	coords = append(coords, b.current.data...)
+	return NewMultiPointFlat(b.layout, coords), nil
+}
+func (b *Builder) LineString() (line *LineString, err *BuilderError) {
+	b.validateGeomType("LineString", lineType)
+	if b.err != nil {
+		return nil, b.err
+	}
 	return NewLineStringFlat(b.layout, b.current.data), nil
 }
 func (b *Builder) LinearRing() (ring *LinearRing, err *BuilderError) {
-	return nil, nil
+	b.validateGeomType("LinearRing", lineType)
+	if b.err != nil {
+		return nil, b.err
+	}
+	b.CloseRing()
+	return NewLinearRingFlat(b.layout, b.current.data), nil
 }
-func (b *Builder) MultiLineString() (lines *MultiLineString, err *BuilderError) {
-	return nil, nil
+func (b *Builder) MultiLineString(allGeoms bool) (lines *MultiLineString, err *BuilderError) {
+	if b.err != nil {
+		return nil, b.err
+	}
+
+	i := 0
+	if !allGeoms {
+		i = len(b.parts)
+		for ; i >= 1 && b.parts[i-1].partType == lineType; i-- {
+		}
+	}
+	numGeoms := len(b.parts) - i + 1
+	coords := make([]float64, 0, numGeoms*b.layout.Stride())
+	ends := make([]int, 0, numGeoms)
+
+	for ; i < len(b.parts); i++ {
+		if b.parts[i].partType != lineType {
+			b.err = &BuilderError{fmt.Sprintf("The geometry at index %d was not a line it was a '%v'", i, b.parts[i].partType), debug.Stack()}
+			return nil, b.err
+		}
+		coords = append(coords, b.parts[i].data...)
+		ends = append(ends, len(coords))
+	}
+
+	b.validateGeomType("Line", lineType)
+
+	if b.err != nil {
+		return nil, b.err
+	}
+
+	coords = append(coords, b.current.data...)
+	ends = append(ends, len(coords))
+	return NewMultiLineStringFlat(b.layout, coords, ends), nil
 }
 func (b *Builder) Polygon() (poly *Polygon, err *BuilderError) {
-	return nil, nil
+	b.validateGeomType("Polygon", polygonType)
+	if b.err != nil {
+		return nil, b.err
+	}
+	b.CloseRing()
+	return NewPolygonFlat(b.layout, b.current.data, b.current.ends), nil
 }
-func (b *Builder) MultiPolygon() (polys *MultiPolygon, err *BuilderError) {
+func (b *Builder) MultiPolygon(allGeoms bool) (polys *MultiPolygon, err *BuilderError) {
 	return nil, nil
 }
 
@@ -155,9 +254,9 @@ func (b *Builder) startGeom(methodName string, partType partType, coord []float6
 	if continueFromLastGeom && len(coord) == 0 {
 		lastPartCoords := b.parts[len(b.parts)-1].data
 		lastCoord := lastPartCoords[len(lastPartCoords)-b.layout.Stride():]
-		b.current = part{partType, lastCoord}
+		b.current = part{partType, lastCoord, []int{}}
 	} else {
-		b.current = part{partType, coord}
+		b.current = part{partType, coord, []int{}}
 	}
 	return b
 }
@@ -175,6 +274,15 @@ func (b *Builder) endGeom() {
 		b.parts = append(b.parts, b.current)
 	}
 	b.current = part{partType: nullType}
+}
+
+func (b *Builder) validateGeomType(humanReadable string, requiredType partType) {
+	if b.err != nil {
+		return
+	}
+	if b.current.partType != requiredType {
+		b.err = &BuilderError{fmt.Sprintf("A %s cannot be created because the current geometry under construction is a '%v'.", humanReadable, b.current.partType), debug.Stack()}
+	}
 }
 
 func stringSlice(coord []float64) string {
@@ -198,3 +306,5 @@ func formatParts(parts []part) string {
 	}
 	return buf.String()
 }
+
+func validateGeomType(requiredType partType) {}
--- builder_test.go
+++ builder_test.go
@@ -1,11 +1,9 @@
 package geom_test
 
 import (
-	"testing"
-
 	"reflect"
-
 	"strings"
+	"testing"
 
 	"github.com/twpayne/go-geom"
 )
@@ -54,7 +52,7 @@ func TestBuilder_Point_MissingGoTo(t *testing.T) {
 	}
 
 	if !strings.Contains(err.Msg, "A point cannot be created") {
-		t.Errorf("geom.Build(geom.XYZ).StartPoint(1, 1).Point() returned the wrong error message: %v", err)
+		t.Errorf("geom.Build(geom.XYZ).Point() returned the wrong error message: %v", err)
 	}
 
 	if point != nil {
@@ -76,6 +74,64 @@ func TestBuilder_Point_LastPoint(t *testing.T) {
 	expectedCoords := []float64{3, 4}
 	if !reflect.DeepEqual(point.FlatCoords(), expectedCoords) {
 		t.Errorf("geom.Build(geom.XY).StartPoint(1, 2).StartPoint(3, 4).Point() has coords: %v wanted: %v", point.FlatCoords(), expectedCoords)
+	}
+}
+
+func TestBuilder_MultiPoint(t *testing.T) {
+	mp, err := geom.Build(geom.XY).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(true)
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint() should not have given an error but got: %v", err)
+	}
+
+	if mp == nil {
+		t.Fatal("geom.Build(geom.XY).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint() should have returned a non-nil point")
+	}
+
+	if mp.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint() has layout %v wanted %v", mp.Layout(), geom.XY)
+	}
+
+	wanted := []float64{1, 1, 3, 3, 2, 2}
+	if !reflect.DeepEqual(mp.FlatCoords(), wanted) {
+		t.Errorf("geom.Build(geom.XY).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint() coords: %v wanted %v", mp.FlatCoords(), wanted)
+	}
+}
+
+func TestBuilder_MultiPoint_IgnoreLine(t *testing.T) {
+	mp, err := geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(false)
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(false) should not have given an error but got: %v", err)
+	}
+
+	if mp == nil {
+		t.Fatal("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(false) should have returned a non-nil geometry")
+	}
+
+	if mp.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(false) has layout %v wanted %v", mp.Layout(), geom.XY)
+	}
+
+	wanted := []float64{1, 1, 3, 3, 2, 2}
+	if !reflect.DeepEqual(mp.FlatCoords(), wanted) {
+		t.Errorf("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(false) coords: %v wanted %v", mp.FlatCoords(), wanted)
+	}
+}
+
+func TestBuilder_MultiPoint_NotAllPoints(t *testing.T) {
+	mp, err := geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(true)
+
+	if err == nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(true) should have given an error")
+	}
+
+	if !strings.Contains(err.Msg, "was not a point") {
+		t.Errorf("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(true) returned the wrong error message: %v", err)
+	}
+
+	if mp != nil {
+		t.Fatal("geom.Build(geom.XY).StartLine(-1, -1).LineTo(-2, -2).StartPoint(1, 1).StartPoint(3, 3).StartPoint(2, 2).MultiPoint(true) should not have geometry")
 	}
 }
 
@@ -150,7 +206,7 @@ func TestBuilder_Line_WrongLayout(t *testing.T) {
 	}
 
 	if !strings.Contains(err.Msg, "correct number of ordinates") {
-		t.Errorf("geom.Build(geom.XYZ).StartPoint(1, 1).Point() returned the wrong error message: %v", err)
+		t.Errorf("geom.Build(geom.XYZ).StartLine(1, 1).LineTo(10, 10).LineString() returned the wrong error message: %v", err)
 	}
 
 	if line != nil {
@@ -166,10 +222,188 @@ func TestBuilder_Line_NoCoordsOnStart(t *testing.T) {
 	}
 
 	if !strings.Contains(err.Msg, "geometry has already been created") {
-		t.Errorf("geom.Build(geom.XYZ).StartPoint(1, 1).Point() returned the wrong error message: %v", err)
+		t.Errorf("geom.Build(geom.XY).StartLine().LineTo(10, 10).LineString() returned the wrong error message: %v", err)
 	}
 
 	if line != nil {
 		t.Fatalf("geom.Build(geom.XYZ).StartLine().LineTo(10, 10).LineString() should not have produced line. Got: %v", line)
+	}
+}
+
+func TestBuilder_Close(t *testing.T) {
+	line, err := geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString()
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() should not have produced an error: %v", err)
+	}
+
+	if line == nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() should have produced line but did not")
+	}
+
+	if line.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() produced a line with the wrong layout.  Was %v wanted %v", line.Layout(), geom.XY)
+	}
+
+	expectedCoords := []float64{1, 1, 10, 10, 10, 20, 1, 1}
+	if !reflect.DeepEqual(line.FlatCoords(), expectedCoords) {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() produced a line with the wrong coords.  Was %v wanted %v", line.FlatCoords(), expectedCoords)
+	}
+}
+
+func TestBuilder_Close_OnlyCloseWhenRequired(t *testing.T) {
+	line, err := geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).LineTo(1, 1).CloseRing().LineString()
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() should not have produced an error: %v", err)
+	}
+
+	if line == nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() should have produced line but did not")
+	}
+
+	if line.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() produced a line with the wrong layout.  Was %v wanted %v", line.Layout(), geom.XY)
+	}
+
+	expectedCoords := []float64{1, 1, 10, 10, 10, 20, 1, 1}
+	if !reflect.DeepEqual(line.FlatCoords(), expectedCoords) {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 20).CloseRing().LineString() produced a line with the wrong coords.  Was %v wanted %v", line.FlatCoords(), expectedCoords)
+	}
+}
+
+func TestBuilder_Close_InsufficientCoords(t *testing.T) {
+	line, err := geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).CloseRing().LineString()
+
+	if err == nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).CloseRing().LineString() should have produced an error")
+	}
+
+	if !strings.Contains(err.Msg, "at least 3 coordinates") {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).CloseRing().LineString() returned the wrong error message: %v", err)
+	}
+
+	if line != nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).CloseRing().LineString() should not have produced line: %v", line)
+	}
+}
+
+func TestBuilder_Close_NeedsStart(t *testing.T) {
+	line, err := geom.Build(geom.XY).CloseRing().LineString()
+
+	if err == nil {
+		t.Fatalf("geom.Build(geom.XY).CloseRing().LineString() should have produced an error")
+	}
+
+	if !strings.Contains(err.Msg, "must be started") {
+		t.Errorf("geom.Build(geom.XY).CloseRing().LineString() returned the wrong error message: %v", err)
+	}
+
+	if line != nil {
+		t.Fatalf("geom.Build(geom.XY).CloseRing().LineString() should not have produced line: %v", line)
+	}
+}
+
+func TestBuilder_LinearRing(t *testing.T) {
+	line, err := geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 15).LinearRing()
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 15).LinearRing() should not have produced an error: %v", err)
+	}
+
+	if line == nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 15).LinearRing() should have produced line but did not")
+	}
+
+	if line.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 15).LinearRing() produced a line with the wrong layout.  Was %v wanted %v", line.Layout(), geom.XY)
+	}
+
+	expectedCoords := []float64{1, 1, 10, 10, 10, 15, 1, 1}
+	if !reflect.DeepEqual(line.FlatCoords(), expectedCoords) {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 10).LineTo(10, 15).LinearRing() produced a line with the wrong coords.  Was %v wanted %v", line.FlatCoords(), expectedCoords)
+	}
+}
+
+func TestBuilder_AddLineSegments(t *testing.T) {
+	line, err := geom.Build(geom.XY).StartLine(1, 1).AddLineSegments(geom.Coord{10, 10}, geom.Coord{10, 20}).LineString()
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).AddLineSegments(geom.Coord{10, 10}, geom.Coord{10, 20}).LineString() should not have produced an error: %v", err)
+	}
+
+	if line == nil {
+		t.Fatalf("geom.Build(geom.XY).StartLine(1, 1).AddLineSegments(geom.Coord{10, 10}, geom.Coord{10, 20}).LineString() should have produced line but did not")
+	}
+
+	if line.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).AddLineSegments(geom.Coord{10, 10}, geom.Coord{10, 20}).LineString() produced a line with the wrong layout.  Was %v wanted %v", line.Layout(), geom.XY)
+	}
+
+	expectedCoords := []float64{1, 1, 10, 10, 10, 20}
+	if !reflect.DeepEqual(line.FlatCoords(), expectedCoords) {
+		t.Errorf("geom.Build(geom.XY).StartLine(1, 1).AddLineSegments(geom.Coord{10, 10}, geom.Coord{10, 20}).LineString() produced a line with the wrong coords.  Was %v wanted %v", line.FlatCoords(), expectedCoords)
+	}
+}
+
+func TestBuilder_MultiLineString(t *testing.T) {
+	ml, err := geom.Build(geom.XY).StartLine(1, 1).LineTo(10, 0).LineTo(10, 10).
+		StartLine(2, 2).LineTo(2, 4).LineTo(4, 4).
+		MultiLineString(false)
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY)....MultiLineString() should not have produced an error: %v", err)
+	}
+
+	if ml == nil {
+		t.Fatalf("geom.Build(geom.XY).....MultiLineString() should have produced MultiLineString but did not")
+	}
+
+	if ml.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).....MultiLineString() produced a MultiLineString with the wrong layout.  Was %v wanted %v", ml.Layout(), geom.XY)
+	}
+
+	expectedCoords := []float64{
+		1, 1, 10, 0, 10, 10,
+		2, 2, 2, 4, 4, 4}
+
+	if !reflect.DeepEqual(ml.FlatCoords(), expectedCoords) {
+		t.Errorf("geom.Build(geom.XY).....MultiLineString() produced a MultiLineString with the wrong coords.  Was %v wanted %v", ml.FlatCoords(), expectedCoords)
+	}
+	expectedEnds := []int{6, 12}
+	if !reflect.DeepEqual(ml.Ends(), expectedEnds) {
+		t.Errorf("geom.Build(geom.XY).....MultiLineString() produced a MultiLineString with the wrong ends.  Was %v wanted %v", ml.Ends(), expectedEnds)
+	}
+}
+
+func TestBuilder_Polygon(t *testing.T) {
+	polygon, err := geom.Build(geom.XY).StartPolygon(1, 1).LineTo(10, 0).LineTo(10, 10).
+		StartHole(2, 2).LineTo(2, 4).LineTo(4, 4).
+		StartHole(6, 6).LineTo(6, 8).LineTo(8, 8).
+		Polygon()
+
+	if err != nil {
+		t.Fatalf("geom.Build(geom.XY)....Polygon() should not have produced an error: %v", err)
+	}
+
+	if polygon == nil {
+		t.Fatalf("geom.Build(geom.XY).....Polygon() should have produced polygon but did not")
+	}
+
+	if polygon.Layout() != geom.XY {
+		t.Errorf("geom.Build(geom.XY).....Polygon() produced a polygon with the wrong layout.  Was %v wanted %v", polygon.Layout(), geom.XY)
+	}
+
+	expectedCoords := []float64{
+		1, 1, 10, 0, 10, 10, 1, 1,
+		2, 2, 2, 4, 4, 4, 2, 2,
+		6, 6, 6, 8, 8, 8, 6, 6}
+
+	if !reflect.DeepEqual(polygon.FlatCoords(), expectedCoords) {
+		t.Errorf("geom.Build(geom.XY).....Polygon() produced a polygon with the wrong coords.  Was %v wanted %v", polygon.FlatCoords(), expectedCoords)
+	}
+	expectedEnds := []int{8, 16, 24}
+	if !reflect.DeepEqual(polygon.Ends(), expectedEnds) {
+		t.Errorf("geom.Build(geom.XY).....Polygon() produced a polygon with the wrong ends.  Was %v wanted %v", polygon.Ends(), expectedEnds)
 	}
 }
